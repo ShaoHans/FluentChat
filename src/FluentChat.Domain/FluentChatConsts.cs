d546240a5c99ec3edb934c97cf036aed73bf168e
ShaoHans <shaomenghao@hotmail.com>
2024-11-10 09:01:00 +0800

    update Admin Default Email and Password

--- src/FluentChat.Domain/FluentChatConsts.cs
+++ src/FluentChat.Domain/FluentChatConsts.cs
@@ -1,11 +1,9 @@
-﻿using Volo.Abp.Identity;
-
-namespace FluentChat;
+﻿namespace FluentChat;
 
 public static class FluentChatConsts
 {
     public const string DbTablePrefix = "App";
     public const string? DbSchema = null;
-    public const string AdminEmailDefaultValue = IdentityDataSeedContributor.AdminEmailDefaultValue;
-    public const string AdminPasswordDefaultValue = IdentityDataSeedContributor.AdminPasswordDefaultValue;
+    public const string AdminEmailDefaultValue = "admin@fluentchat.ai";
+    public const string AdminPasswordDefaultValue = "$20241109Start*";
 }
