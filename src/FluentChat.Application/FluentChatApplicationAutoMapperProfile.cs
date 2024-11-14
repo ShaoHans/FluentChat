b8e4b9b251d7e6e818700667c710731f97077d09
ShaoHans <shaomenghao@hotmail.com>
2024-11-14 22:41:40 +0800

    Dto Mapper

--- src/FluentChat.Application/FluentChatApplicationAutoMapperProfile.cs
+++ src/FluentChat.Application/FluentChatApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using FluentChat.Chat;
 using FluentChat.Chats.Dtos;
 using FluentChat.Models;
 
@@ -8,7 +9,33 @@ public class FluentChatApplicationAutoMapperProfile : Profile
 {
     public FluentChatApplicationAutoMapperProfile()
     {
-        CreateMap<CreateSessionDto, ChatSession>();
-        CreateMap<CreateMessageDto, ChatMessage>();
+        CreateMap<CreateSessionDto, ChatSession>()
+            .ForMember(
+                dest => dest.Title,
+                opt =>
+                    opt.MapFrom(src =>
+                        src.Title.Substring(0, ChatConsts.ChatSession_Title_MaxLength)
+                    )
+            )
+            .ForMember(
+                dest => dest.Service,
+                opt =>
+                    opt.MapFrom(src =>
+                        src.Service.Substring(0, ChatConsts.ChatSession_Service_MaxLength)
+                    )
+            )
+            .ForMember(
+                dest => dest.Model,
+                opt =>
+                    opt.MapFrom(src =>
+                        src.Model.Substring(0, ChatConsts.ChatSession_Model_MaxLength)
+                    )
+            );
+        CreateMap<CreateMessageDto, ChatMessage>()
+            .ForMember(
+                dest => dest.Role,
+                opt =>
+                    opt.MapFrom(src => src.Role.Substring(0, ChatConsts.ChatMessage_Role_MaxLength))
+            );
     }
 }
