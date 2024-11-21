ff38146b2a22911fa836b3fa256b2414b8a7949b
Hans <shaomenghao@hotmail.com>
2024-11-21 18:24:52 +0800

    add AIModel class

---
+++ src/FluentChat.Domain/Models/AIModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+using Volo.Abp.MultiTenancy;
+
+namespace FluentChat.Models;
+
+public class AIModel : FullAuditedAggregateRoot<Guid>, IMultiTenant
+{
+    public Guid? TenantId { get; set; }
+
+    public string Provider { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+}
